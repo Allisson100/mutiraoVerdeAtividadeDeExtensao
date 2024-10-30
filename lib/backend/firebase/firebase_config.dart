bea375af5f593d5e86c55713e01e36666630ae14
Allisson100 <allisson.boiane@outlook.com>
2024-10-30 17:51:49 -0300

    first commit

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,12 +5,12 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyARkbjIoxZkdFZs2099OCUG0ILxcYw8JQg",
-            authDomain: "limpeza-2a83b.firebaseapp.com",
-            projectId: "limpeza-2a83b",
-            storageBucket: "limpeza-2a83b.firebasestorage.app",
-            messagingSenderId: "674861373433",
-            appId: "1:674861373433:web:5a002b2f4af5c985910f28"));
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: ""));
   } else {
     await Firebase.initializeApp();
   }
